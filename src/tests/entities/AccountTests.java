1d711f357c3921ec615c403ea3057d72225db9a9
EwertonILima <ewertoninaciolima@gmail.com>
2021-06-05 19:36:16 -0300

    First test with JUnit

---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,32 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+
+	@Test
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+
+		acc.deposit(amount);
+
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+}
